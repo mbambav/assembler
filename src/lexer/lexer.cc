eeecf39c79813f28acecb33435412fe3dc6c9494
mbambav <goyalarnav36@gmail.com>
2025-02-03 15:36:04 -0500

    update: added tokenization for semicolons to signify label end

--- src/lexer/lexer.cc
+++ src/lexer/lexer.cc
@@ -1,251 +1,289 @@
 #include "lexer.hh"
-
+// #include"../parser/parser.hh"
 #include <filesystem>
 #include <iostream>
+#include <type_traits>
 
-namespace Lexer {
-Lexer::Lexer(std::string_view file) {
-    this->file = file;
-    std::filesystem::path pathobj(file);
-    std::string           file_name = pathobj.filename().string();
-    std::wstring          widestr   = std::wstring(file_name.begin(), file_name.end());
+namespace assembler {
+namespace lexer {
+    Lexer::Lexer(std::string_view file) {
+        this->file = file;
+        std::filesystem::path pathobj(file);
+        std::string           file_name = pathobj.filename().string();
+        std::wstring          widestr   = std::wstring(file_name.begin(), file_name.end());
 #if defined(__APPLE__) || defined(__linux__) || defined(__unix__) || defined(__MACH__)
-    // read the contents of the file and store it in data
-    int fd = open(file.data(), O_RDONLY);
-    if (fd == -1) {
-        std::cerr << "failed to open file\n";
-        std::exit(1000);
-    }
+        // read the contents of the file and store it in data
+        int fd = open(file.data(), O_RDONLY);
+        if (fd == -1) {
+            std::cerr << "failed to open file\n";
+            std::exit(1000);
+        }
 
-    off_t file_size = lseek(fd, 0, SEEK_END);
-    if (file_size == -1) {
-        close(fd);
-        std::cerr << "failed to get file size\n";
-        std::exit(2000);
-    }
+        off_t file_size = lseek(fd, 0, SEEK_END);
+        if (file_size == -1) {
+            close(fd);
+            std::cerr << "failed to get file size\n";
+            std::exit(2000);
+        }
 
-    void *map = mmap(nullptr, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (map == MAP_FAILED) {
+        void *map = mmap(nullptr, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        if (map == MAP_FAILED) {
+            close(fd);
+            std::cerr << "failed to map\n";
+            std::exit(3000);
+        }
         close(fd);
-        std::cerr << "failed to map\n";
-        std::exit(3000);
-    }
-    close(fd);
 #elif defined(_WIN32)
-    HANDLE file_handle = CreateFileW(
-        widestr.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+        HANDLE file_handle = CreateFileW(widestr.c_str(),
+                                         GENERIC_READ,
+                                         0,
+                                         nullptr,
+                                         OPEN_EXISTING,
+                                         FILE_ATTRIBUTE_NORMAL,
+                                         nullptr);
 
-    if (file_handle == INVALID_HANDLE_VALUE) {
-        std::cerr << "Failed to open file\n";
-        std::exit(100);
-    }
+        if (file_handle == INVALID_HANDLE_VALUE) {
+            std::cerr << "Failed to open file\n";
+            std::exit(100);
+        }
 
-    LARGE_INTEGER size;
-    if (!GetFileSizeEx(file_handle, &size)) {
-        std::cerr << "could not get file size\n";
-        CloseHandle(file_handle);
-        std::exit(200);
-    }
+        LARGE_INTEGER size;
+        if (!GetFileSizeEx(file_handle, &size)) {
+            std::cerr << "could not get file size\n";
+            CloseHandle(file_handle);
+            std::exit(200);
+        }
 
-    size_t file_size = static_cast<size_t>(size.QuadPart);
+        size_t file_size = static_cast<size_t>(size.QuadPart);
 
-    HANDLE file_mapping = CreateFileMapping(file_handle, nullptr, PAGE_READONLY, 0, 0, nullptr);
+        HANDLE file_mapping = CreateFileMapping(file_handle, nullptr, PAGE_READONLY, 0, 0, nullptr);
 
-    if (!file_mapping) {
-        std::cerr << "could not create file mapping\n";
-        CloseHandle(file_handle);
-        std::exit(300);
-    }
+        if (!file_mapping) {
+            std::cerr << "could not create file mapping\n";
+            CloseHandle(file_handle);
+            std::exit(300);
+        }
 
-    void *map = MapViewOfFile(file_mapping, FILE_MAP_READ, 0, 0, 0);
+        void *map = MapViewOfFile(file_mapping, FILE_MAP_READ, 0, 0, 0);
 
-    if (!map) {
-        std::cerr << "failed to map view of file\n";
+        if (!map) {
+            std::cerr << "failed to map view of file\n";
+            CloseHandle(file_handle);
+            CloseHandle(file_mapping);
+            std::exit(400);
+        }
+
         CloseHandle(file_handle);
         CloseHandle(file_mapping);
-        std::exit(400);
-    }
-
-    CloseHandle(file_handle);
-    CloseHandle(file_mapping);
 
 #else
-    std::cerr << "unknow operating system\n";
-    std::exit(1);
+        std::cerr << "unknow operating system\n";
+        std::exit(1);
 #endif
 
-    this->data = std::string_view(static_cast<const char *>(map), file_size);
-}
+        this->data = std::string_view(static_cast<const char *>(map), file_size);
+    }
 
-TokenList Lexer::tokenize() {
-    TokenList tokens;
-    // check if next char is !\n
-    // add to word
-    // check if it is a tokentype
-    // check if it is valid tokentype
-    size_t      i          = 0;
-    size_t      max_length = data.size();
-    std::string word       = "";
+    TokenList Lexer::tokenize() {
+        TokenList tokens;
+        // check if next char is !\n
+        // add to word
+        // check if it is a tokentype
+        // check if it is valid tokentype
+        size_t      i          = 0;
+        size_t      max_length = data.size();
+        std::string word       = "";
 
-    for (; i < max_length; i++) {
-        char cur_ch = data[i];
+        for (; i < max_length; i++) {
+            char cur_ch = data[i];
 
-        switch (cur_ch) {
-            case ',': {
-                if (word.empty()) {
-                    break;
-                }
-                tokens.emplace_back(get_token_type(word));
-                tokens.emplace_back(get_token_type(","));
-                word = "";
-                break;
-            }
-            case ' ': {
-                if (word.empty()) {
-                    break;
-                }
-                tokens.emplace_back(get_token_type(word));
-                word = "";
-                break;
-            }
-            case '\r': {
-            line_ending_handling:
-                if (i + 1 > max_length || data[i + 1] != '\n') {
-                    word += cur_ch;
-                }
-                break;
-            }
-            case '\n': {
-                if (word.empty()) {
-                    break;
-                }
-                tokens.emplace_back(get_token_type(word));
-                word = "";
-                break;
-            }
-            case '/': {
-                if (i + 1 > max_length && data[i + 1] != '/') {
-                    break;
-                }
-                while (i < max_length) {
-                    cur_ch = data[i];
-                    if (cur_ch == '\n' || cur_ch == '\r') {
-                        goto line_ending_handling;
+            switch (cur_ch) {
+                case ',': {
+                    if (word.empty()) {
                         break;
                     }
+                    tokens.emplace_back(get_token_type(word));
+                    tokens.emplace_back(get_token_type(","));
+                    word = "";
+                    break;
+                }
+                case ' ': {
+                    if (word.empty()) {
+                        break;
+                    }
+                    tokens.emplace_back(get_token_type(word));
+                    word = "";
+                    break;
+                }
+                case '\r': {
+                line_ending_handling:
+                    if (i + 1 > max_length || data[i + 1] != '\n') {
+                        word += cur_ch;
+                    }
+                    break;
+                }
+                case '\n': {
+                    if (word.empty()) {
+                        break;
+                    }
+                    tokens.emplace_back(get_token_type(word));
+                    tokens.emplace_back(set_new_line());
+                    word = "";
+                    break;
+                }
+                case '/': {
+                    if (i + 1 > max_length && data[i + 1] != '/') {
+                        break;
+                    }
+                    while (i < max_length) {
+                        cur_ch = data[i];
+                        if (cur_ch == '\n' || cur_ch == '\r') {
+                            goto line_ending_handling;
+                            break;
+                        }
 
-                    word += cur_ch;
-                    ++i;
+                        word += cur_ch;
+                        ++i;
+                    }
+
+                    tokens.emplace_back(get_token_type(word));
+                    word = "";
+                    break;
                 }
 
-                tokens.emplace_back(get_token_type(word));
-                word = "";
-                break;
-            }
-
-            default:
-                word += cur_ch;
-                break;
-        }
-    }
-    if (!word.empty()) {
-        tokens.emplace_back(get_token_type(word));
-    }
-
-    std::cout << "comment type: " << std::to_string(int(mapping["comment"])) << "\n";
-    std::cout << "comment type: " << std::to_string(int(mapping["comment"])) << "\n";
-    for (auto &i : tokens) {
-        std::cout << "tok: " << std::to_string(int(i.type)) << " \"" << std::string(i.data)
-                  << "\"\n";
-    }
-    return tokens;
-}
-
-Token Lexer::get_token_type(const std::string &word) {
-    auto             elm  = mapping.find(word);
-    size_t           line = 0;
-    size_t           col  = 0;
-    std::string_view file = "";
-
-    if (elm != mapping.end()) {
-        return {.data = word, .file = file, .line = line, .column = col, .type = elm->second};
-    } else {
-        for (auto &literal : literals) {
-            if (std::regex_match(word, literal)) {
-                return {.data   = word,
-                        .file   = file,
-                        .line   = line,
-                        .column = col,
-                        .type   = TokenType::LITERAL};
+                default:
+                    word += cur_ch;
+                    break;
             }
         }
-
-        for (auto &register_ : registers) {
-            if (std::regex_match(word, register_)) {
-                return {.data   = word,
-                        .file   = file,
-                        .line   = line,
-                        .column = col,
-                        .type   = TokenType::REGISTER};
-            }
-        }
-        for (auto &comma : commas) {
-            if (std::regex_match(word, comma)) {
-                return {.data   = word,
-                        .file   = file,
-                        .line   = line,
-                        .column = col,
-                        .type   = TokenType::COMMA};
-            }
+        if (!word.empty()) {
+            tokens.emplace_back(get_token_type(word));
         }
 
-        for (auto &string_ : strings) {
-            if (std::regex_match(word, string_)) {
-                return {.data   = word.substr(1, word.length() - 2),
-                        .file   = file,
-                        .line   = line,
-                        .column = col,
-                        .type   = TokenType::STRING};
-            }
-        }
+        std::cout << "comment type: " << std::to_string(int(mapping["comment"])) << "\n";
+        std::cout << "comma type: " << std::to_string(int(mapping["comma"])) << "\n";
 
-        for (auto &label : labels) {
-            if (std::regex_match(word, label)) {
-                return {.data   = word.substr(0, word.length() - 1),
-                        .file   = file,
-                        .line   = line,
-                        .column = col,
-                        .type   = TokenType::LABEL};
-            }
+        for (auto &i : tokens) {
+            std::cout << "tok: " << std::to_string(int(i.type)) << " \"" << std::string(i.data)
+                      << "\" " << (i.opcode) << "\n";
         }
-
-        for(auto &label_call : label_calls) {
-            if (std::regex_match(word, label_call)) {
-                return {.data   = word,
-                        .file   = file,
-                        .line   = line,
-                        .column = col,
-                        .type   = TokenType::LABEL_CALL};
-            }
-        }
-
-        for (auto &comment : comments) {
-            if (std::regex_match(word, comment)) {
-                return {.data   = word,
-                        .file   = file,
-                        .line   = line,
-                        .column = col,
-                        .type   = TokenType::COMMENT};
-            }
-        }
+        return tokens;
     }
 
-    return {.data = word, .file = file, .line = line, .column = col, .type = TokenType::UNKNOWN};
-}
-}  // namespace Lexer
+    Token Lexer::get_token_type(const std::string &word) {
+        auto             elm  = mapping.find(word);
+        size_t           line = 0;
+        size_t           col  = 0;
+        std::string_view file = "";
+
+        if (elm != mapping.end()) {
+            return {.data   = word,
+                    .file   = file,
+                    .line   = line,
+                    .column = col,
+                    .type   = elm->second,
+                    .opcode = true};
+        } else {
+            for (auto &literal : literals) {
+                if (std::regex_match(word, literal)) {
+                    return {.data   = word,
+                            .file   = file,
+                            .line   = line,
+                            .column = col,
+                            .type   = TokenType::LITERAL,
+                            .opcode = false};
+                }
+            }
+
+            for (auto &register_ : registers) {
+                if (std::regex_match(word, register_)) {
+                    return {.data   = word,
+                            .file   = file,
+                            .line   = line,
+                            .column = col,
+                            .type   = TokenType::REGISTER,
+                            .opcode = false};
+                }
+            }
+            for (auto &comma : commas) {
+                if (std::regex_match(word, comma)) {
+                    return {.data   = word,
+                            .file   = file,
+                            .line   = line,
+                            .column = col,
+                            .type   = TokenType::COMMA,
+                            .opcode = false};
+                }
+            }
+
+            for (auto &string_ : strings) {
+                if (std::regex_match(word, string_)) {
+                    return {.data   = word.substr(1, word.length() - 2),
+                            .file   = file,
+                            .line   = line,
+                            .column = col,
+                            .type   = TokenType::STRING,
+                            .opcode = false};
+                }
+            }
+
+            for (auto &label : labels) {
+                if (std::regex_match(word, label)) {
+                    return {.data   = word.substr(0, word.length() - 1),
+                            .file   = file,
+                            .line   = line,
+                            .column = col,
+                            .type   = TokenType::LABEL,
+                            .opcode = false};
+                }
+            }
+
+            for (auto &label_call : label_calls) {
+                if (std::regex_match(word, label_call)) {
+                    return {.data   = word,
+                            .file   = file,
+                            .line   = line,
+                            .column = col,
+                            .type   = TokenType::LABEL_CALL,
+                            .opcode = false};
+                }
+            }
+
+            for (auto &comment : comments) {
+                if (std::regex_match(word, comment)) {
+                    return {.data   = word,
+                            .file   = file,
+                            .line   = line,
+                            .column = col,
+                            .type   = TokenType::COMMENT,
+                            .opcode = false};
+                }
+            }
+        }
+
+        return {.data   = word,
+                .file   = file,
+                .line   = line,
+                .column = col,
+                .type   = TokenType::UNKNOWN,
+                .opcode = false};
+    }
+
+    Token Lexer::set_new_line() {
+        line   = 0;
+        column = 0;
+        file   = "";
+        return {.data   = "newline",
+                .file   = file,
+                .line   = line,
+                .column = column,
+                .type   = TokenType::NEWLINE};
+    }
+}  // namespace lexer
+}  // namespace assembler
 
 int main() {
-    Lexer::Lexer lexer("/Volumes/Projects/c++/test.txt");
+    assembler::lexer::Lexer lexer("/Volumes/Projects/assembly-lang/test.txt");
     lexer.tokenize();
     return 0;
 }
--- src/lexer/lexer.hh
+++ src/lexer/lexer.hh
@@ -1,14 +1,13 @@
 #ifndef TOKEN_TYPES_HH
 #define TOKEN_TYPES_HH
 
+#include <fcntl.h>
+
 #include <map>
 #include <regex>
 #include <string>
 #include <string_view>
 #include <vector>
-#include <fcntl.h>
-
-
 
 #ifdef _WIN32
 #include <handleapi.h>
@@ -21,157 +20,130 @@
 
 #include <cstdio>
 
+namespace assembler {
+namespace lexer {
+    struct Token;
+    using TokenList = std::vector<Token>;
 
-namespace Lexer {
-struct Token;
-using TokenList = std::vector<Token>;
+    enum class TokenType {
+        LOAD,
+        STORE,
+        MOVE,
+        PUSH,
+        POP,
+        SWAP,
+        ADD,
+        SUB,
+        MUL,
+        DIV,
+        MOD,
+        INC,
+        DEC,
+        AND,
+        OR,
+        XOR,
+        NOT,
+        SHL,
+        SHR,
+        CMP,
+        EQ,
+        NE,
+        JMP,
+        JE,
+        JNE,
+        JG,
+        JGE,
+        JL,
+        JLE,
+        WHILE,
+        CALL,
+        RET,
+        ENTER,
+        LEAVE,
+        NOP,
+        STOP,
+        DEBUG,
+        SYS,
+        LITERAL,
+        REGISTER,
+        NEWLINE,
+        SEMICOLON,
+        COMMA,
+        STRING,
+        COMMENT,
+        LABEL,
+        LABEL_CALL,
+        UNKNOWN
+    };
 
-enum class TokenType {
-    LOAD,
-    STORE,
-    MOVE,
-    PUSH,
-    POP,
-    SWAP,
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    MOD,
-    INC,
-    DEC,
-    AND,
-    OR,
-    XOR,
-    NOT,
-    SHL,
-    SHR,
-    CMP,
-    EQ,
-    NE,
-    JMP,
-    JE,
-    JNE,
-    JG,
-    JGE,
-    JL,
-    JLE,
-    WHILE,
-    CALL,
-    RET,
-    ENTER,
-    LEAVE,
-    NOP,
-    STOP,
-    DEBUG,
-    SYS,
-    LITERAL,
-    REGISTER,
-    COMMA,
-    STRING,
-    COMMENT,
-    LABEL,
-    LABEL_CALL,
-    UNKNOWN
-};
+    inline std::map<std::string, TokenType> mapping{
+        {"load", TokenType::LOAD},   {"store", TokenType::STORE}, {"move", TokenType::MOVE},
+        {"push", TokenType::PUSH},   {"pop", TokenType::POP},     {"swap", TokenType::SWAP},
+        {"add", TokenType::ADD},     {"sub", TokenType::SUB},     {"mul", TokenType::MUL},
+        {"div", TokenType::DIV},     {"mod", TokenType::MOD},     {"inc", TokenType::INC},
+        {"dec", TokenType::DEC},     {"and", TokenType::AND},     {"or", TokenType::OR},
+        {"xor", TokenType::XOR},     {"not", TokenType::NOT},     {"shl", TokenType::SHL},
+        {"shr", TokenType::SHR},     {"cmp", TokenType::CMP},     {"eq", TokenType::EQ},
+        {"ne", TokenType::NE},       {"jmp", TokenType::JMP},     {"je", TokenType::JE},
+        {"jne", TokenType::JNE},     {"jg", TokenType::JG},       {"jge", TokenType::JGE},
+        {"jl", TokenType::JL},       {"jle", TokenType::JLE},     {"while", TokenType::WHILE},
+        {"call", TokenType::CALL},   {"ret", TokenType::RET},     {"enter", TokenType::ENTER},
+        {"leave", TokenType::LEAVE}, {"nop", TokenType::NOP},     {"stop", TokenType::STOP},
+        {"debug", TokenType::DEBUG}, {"sys", TokenType::SYS},     {"newLine", TokenType::NEWLINE},
+        {";", TokenType::SEMICOLON}};
 
-std::map<std::string, TokenType> mapping{
-    {"load", TokenType::LOAD},
-    {"store", TokenType::STORE},
-    {"move", TokenType::MOVE},
-    {"push", TokenType::PUSH},
-    {"pop", TokenType::POP},
-    {"swap", TokenType::SWAP},
-    {"add", TokenType::ADD},
-    {"sub", TokenType::SUB},
-    {"mul", TokenType::MUL},
-    {"div", TokenType::DIV},
-    {"mod", TokenType::MOD},
-    {"inc", TokenType::INC},
-    {"dec", TokenType::DEC},
-    {"and", TokenType::AND},
-    {"or", TokenType::OR},
-    {"xor", TokenType::XOR},
-    {"not", TokenType::NOT},
-    {"shl", TokenType::SHL},
-    {"shr", TokenType::SHR},
-    {"cmp", TokenType::CMP},
-    {"eq", TokenType::EQ},
-    {"ne", TokenType::NE},
-    {"jmp", TokenType::JMP},
-    {"je", TokenType::JE},
-    {"jne", TokenType::JNE},
-    {"jg", TokenType::JG},
-    {"jge", TokenType::JGE},
-    {"jl", TokenType::JL},
-    {"jle", TokenType::JLE},
-    {"while", TokenType::WHILE},
-    {"call", TokenType::CALL},
-    {"ret", TokenType::RET},
-    {"enter", TokenType::ENTER},
-    {"leave", TokenType::LEAVE},
-    {"nop", TokenType::NOP},
-    {"stop", TokenType::STOP},
-    {"debug", TokenType::DEBUG},
-    {"sys", TokenType::SYS},
-    {"literal", TokenType::LITERAL},
-    {"register", TokenType::REGISTER},
-    {"string", TokenType::STRING},
-    {"comma", TokenType::COMMA},
-    {"comment", TokenType::COMMENT},
-    {"label", TokenType::LABEL},
-    {"label_call", TokenType::LABEL_CALL},
-    {"unknown", TokenType::UNKNOWN},
-};
+    struct Token {
+        std::string      data;
+        std::string_view file;
+        size_t           line;
+        size_t           column;
+        TokenType        type;
+        bool opcode;
+        // copy assignment
+        Token &operator=(const Token &token) = default;
+    };
 
+    class Lexer {
+      private:
+        std::string_view file;
+        std::string_view data;
 
-struct Token {
-    const std::string data;
-    const std::string_view file;
-    size_t                 line;
-    size_t                 column;
-    TokenType              type;
-};
+        size_t line;
+        size_t column;
+        size_t offset;
 
-class Lexer {
-  private:
-    std::string_view file;
-    std::string_view data;
+      public:
+        Lexer(std::string_view file);
+        TokenList tokenize();
+        Token     get_token_type(const std::string &word);
+        Token     set_new_line();
+    };
 
-    size_t line;
-    size_t column;
-    size_t offset;
+    // define literals
+    const std::regex literals[] = {
+        std::regex("[0-9]+"),
+        std::regex("0x[0-9a-fA-F]+"),
+        std::regex("0b[01]+"),
+    };
 
-  public:
-    Lexer(std::string_view file);
-    TokenList tokenize();
-    Token get_token_type(const std::string& word);
-};
+    // define registers
+    const std::regex registers[] = {std::regex("r[0-9]+")};
 
-// define literals
-const std::regex literals[] = {
-    std::regex("[0-9]+"),
-    std::regex("0x[0-9a-fA-F]+"),
-    std::regex("0b[01]+"),
-};
+    // define commas
+    const std::regex commas[] = {std::regex(",")};
 
-// define registers
-const std::regex registers[] = {std::regex("r[0-9]+")};
+    // define strings
+    const std::regex strings[] = {std::regex("\".*\"")};
 
-// define commas
-const std::regex commas[] = {std::regex(",")};
+    // define labels
+    const std::regex labels[] = {std::regex("[a-zA-Z_][a-zA-Z0-9_]*:")};
 
-// define strings
-const std::regex strings[] = {std::regex("\".*\"")};
+    // define label calls. they start with a .
+    const std::regex label_calls[] = {std::regex("\\.[a-zA-Z_][a-zA-Z0-9_]*")};
 
-// define labels
-const std::regex labels[] = {std::regex("[a-zA-Z_][a-zA-Z0-9_]*:")};
+    // define comments using //
+    const std::regex comments[] = {std::regex("//.*")};
 
-//define label calls. they start with a .
-const std::regex label_calls[] = {std::regex("\\.[a-zA-Z_][a-zA-Z0-9_]*")};
-
-// define comments using //
-const std::regex comments[] = {std::regex("//.*")};
-
-}  // namespace Lexer
+}  // namespace lexer
+}  // namespace assembler
 #endif  // !TOKEN_TYPES_HH
